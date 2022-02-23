73a586015370731ddf6f5efff33ac9dc43cb2e43
jasper baijens <jasperbayens@outlook.com>
2022-02-23 13:18:16 +0100

    Opdracht B3

--- WeekOpdrachten/Week4/OpdrachtB3/OpdrachtB3/main.c
+++ WeekOpdrachten/Week4/OpdrachtB3/OpdrachtB3/main.c
@@ -19,8 +19,8 @@ void wait( int ms )
 // Initialize ADC: 10-bits (left justified), free running
 void adcInit( void )
 {
-	ADMUX = 0b01100001;			// AREF=VCC, result left adjusted, channel1 at pin PF1
-	ADCSRA = 0b11100110;		// ADC-enable, no interrupt, start, free running, division by 64
+	ADMUX = 0b01100011;			// AREF=VCC, result left adjusted, channel3 at pin PF3
+	ADCSRA = 0b10000110;		// ADC-enable, no interrupt, no start, no free running, division by 64
 }
 
 
@@ -29,13 +29,15 @@ int main( void )
 {
 	DDRF = 0x00;				// set PORTF for input (ADC)
 	DDRA = 0xFF;				// set PORTA for output
-	DDRB = 0xFF;				// set PORTB for output
 	adcInit();					// initialize ADC
 
 	while (1)
 	{
-		PORTB = ADCL;			// Show MSB/LSB (bit 10:0) of ADC
+		ADCSRA |= BIT(6);
+		while (ADCSRA & BIT(6));
 		PORTA = ADCH;
 		wait(100);				// every 100 ms (busy waiting)
 	}
 }
+
+
